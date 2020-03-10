d78c1bc2d77ec1529c3717190b03e2036fa20b53
Jovan Goh <jovangoh@Jovans-MacBook-Pro.local>
2020-03-10 10:08:48 +0800

    created skeleton react-app

---
+++ src/App.js
@@ -0,0 +1,48 @@
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import NavBar from "./components/NavBar";
+import HomePage from "./pages/HomePage";
+import MainPage from "./pages/MainPage";
+import MyProfilePage from "./pages/MyProfilePage";
+import UserProfile from "./pages/UserProfile";
+import Challenges from "./pages/Challenges";
+import UploadPage from "./pages/UploadPage";
+import LoginForm from "./components/LoginForm";
+import SignUpForm from "./components/SignUpForm";
+// import axios from "axios";
+// import { Route } from "react-router-dom";
+// import { ToastContainer, toast } from "react-toastify";
+// import Image from "react-graceful-image";
+
+function App() {
+  const [users, setUsers] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  // for Reference if you need to use useEffect
+
+  // useEffect(() => {
+  //   axios.get("https://insta.nextacademy.com/api/v1/users/").then(result => {
+  //     setUsers(result.data);
+  //     setIsLoading(false);
+  //   });
+
+  //   let loggedInUser = localStorage.getItem("userInfo");
+  //   if (loggedInUser) {
+  //     setCurrentUser(JSON.parse(loggedInUser));
+  //   }
+  // }, []);
+
+  return (
+    <div className="App">
+      <HomePage />
+      <LoginForm />
+      <SignUpForm />
+      <MainPage />
+      <UserProfile />
+      <MyProfilePage />
+      <Challenges />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/UploadPage.js
@@ -0,0 +1,13 @@
+// optional, only work on this after we completed other features
+
+// import React from 'react'
+
+// const UploadPage = () => {
+//   return (
+//     <div>
+
+//     </div>
+//   )
+// }
+
+// export default UploadPage
