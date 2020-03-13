f2130455342c099acad7ebee32cf2b188882bfa4
Robinahood1311 <59414122+Robinahood1311@users.noreply.github.com>
2020-03-13 10:54:23 +0800

    added user profile

--- src/pages/UserProfile.js
+++ src/pages/UserProfile.js
@@ -10,7 +10,9 @@ import {
   CardSubtitle,
   CardText
 } from "reactstrap";
-// import Image from "react-graceful-image";
+import Image from "react-graceful-image";
+import defaultImage from "../assets/images/default-profile.png"
+import styles from "./UserProfile.module.css"
 
 /*
 // below is the variable for "users" that we fetched from API
@@ -26,22 +28,46 @@ years_of_training:
 
 const UserProfile = ({ users }) => {
   return (
-    <Container>
-      <Row>
-        {users.map(user => {
-          return (
-            <Card>
-              <CardBody>
-                <CardTitle>{user.username}</CardTitle>
-                <CardSubtitle>{user.age}</CardSubtitle>
-                <CardText>{user.height}</CardText>
-              </CardBody>
-            </Card>
-          );
-        })}
-        <NavBar />
-      </Row>
-    </Container>
+    <>
+      <Container fluid={true} style={{ marginBottom: "70px" }}>
+        <Row>
+          {
+            users.map((user, index) => {
+              return (
+
+                <Col key={index} xs="6" md="4">
+                  <div style={{ border: "1px solid black", borderRadius: "10px", margin: '10px -5px' }}>
+                    <Image src={defaultImage} className={styles.profilePic} />
+                    <h4>
+                      {user.username}
+                    </h4>
+                    <h6>
+                      age
+                  </h6>
+                    <p>
+                      weight kg, height cm
+                  </p>
+                    <p><span className={styles.runningSpan}>running</span><span className={styles.pilatesSpan}>pilates</span><span className={styles.yogaSpan}>yoga</span></p>
+                    <p>
+                      <span className={styles.weightSpan}>weight lifting</span>
+                    </p>
+                  </div>
+                </Col>
+
+                // <Card>
+                //   <CardBody>
+                //     <CardTitle>{user.username}</CardTitle>
+                //     <CardSubtitle>{user.age}</CardSubtitle>
+                //     <CardText>{user.height}</CardText>
+                //   </CardBody>
+                // </Card>
+              );
+            })
+          }
+        </Row>
+      </Container >
+      < NavBar />
+    </>
   );
 };
 
